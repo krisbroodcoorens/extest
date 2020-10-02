52b2a7372592d655def1e890085212c012345dd4
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-10-02 14:29:20 +0200

    Exercise A5

---
+++ ExerciseA5/src/test/java/TestPerson.java
@@ -0,0 +1,43 @@
+import be.abis.exercise.model.Person;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import java.time.LocalDate;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class TestPerson
+{
+    @Test
+    public void personShouldBe44YearsOld()
+    {
+        //Arrange
+        int expected = 44;
+        Person myPerson = new Person(1234,"Nathalie","Moerman", LocalDate.of(1976, 6, 28));
+
+        //Act
+        int calculatedAge = myPerson.calculateAge();
+
+        //AssertEquals
+        assertEquals(expected, calculatedAge);
+
+        //AssertThat
+        assertThat(calculatedAge,is(expected));
+
+    }
+
+    @Test
+    public void toStringSentenceStartWithPerson()
+    {
+        //Arrange
+        Person myPerson = new Person(1234,"Nathalie","Moerman", LocalDate.of(1976, 6, 28));
+        String expectedStringName = "Nathalie Moerman";
+        System.out.println(expectedStringName);
+
+        //Act
+        String actualStringName = myPerson.getFirstName() + " " + myPerson.getLastName();
+        System.out.println(actualStringName);
+
+        //AssertThat
+        assertThat(actualStringName, startsWith(expectedStringName));
+    }
+}
